922953209defe9ebe10d3b767b700352910ec994
sigumaa <siguma95@gmail.com>
2020-12-08 20:34:05 +0900

    2

---
+++ 2.cpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+
+Serial pc(USBTX, USBRX);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if((0 <= n)&&(n <= 100)){
+        if(n == 9){
+            printf("n is 9\n");
+            led1 = 1;
+        }
+        else{
+            printf("n is not 9\n");
+            led2 = 1;
+        }
+    }
+    else{
+        printf("なにか間違えてないかな？\n");
+    }
+
+}
