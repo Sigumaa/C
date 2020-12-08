e589441187c4e2c10516a84f8bd4ebee3e9b4e0f
sigumaa <siguma95@gmail.com>
2020-12-08 20:09:28 +0900

    1

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+
+Serial pc(USBTX, USBRX);
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if((2 <= n)&&(n <= 100)){
+        printf("%d\n", n * (n + 1) / 2);
+    }
+    else{
+        printf("なにか間違えてないかな？\n");
+    }
+
+}
