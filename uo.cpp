1f8c4ce8c664a754e7af4b2aead8e6ef1ae26886
sigumaa <siguma95@gmail.com>
2020-12-12 23:11:40 +0900

    難易度上がり過ぎでは...?

---
+++ uo.cpp
@@ -0,0 +1,55 @@
+#include "mbed.h"
+
+Serial pc(USBTX, USBRX);
+
+
+// プロトタイプ宣言
+void swap(int *a, int *b);
+void printArray(int a[3][3]);
+void transposition(int a[3][3]);
+
+// main
+int main() {
+    int a[3][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]); // 読み取る
+        }
+    }
+    printArray(a);// 交換前の、aの各値を出力する
+    transposition(a);// 交換する
+    printf("\n")// 無いと交換前と交換後がつながっちゃう
+    printArray(a);// 交換した後のaの各値を出力する
+
+}
+
+// 交換する
+void swap(int *a, int *b) {
+    int exchange;
+    exchange = *a; //*aを入れておく
+    *a = *b; //*aを*bにする(日本語弱者でごめんなさい)　・・・①
+    *b = exchange;// *bを*aにする　・・・②
+    //①　②・・・入れ替わってる！
+}
+
+
+// aの各値を出力する
+void printArray(int a[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d", a[i][j]);
+        }
+        printf("\n")
+    }
+}
+
+
+// a[i][j]とa[j][i]の値をswapを使って交換する関数(問題文の指示)
+void transposition(int a[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < i; j++) {
+            swap(&a[i][j], &a[j][i]);
+        }
+    }
+}
+
